914b76287a4daf81fabac6162e6002250f197371
aikopsidas <96435309+aikopsidas@users.noreply.github.com>
2022-03-19 22:17:52 -0500

    adding challeng data

--- 11.3/static/app_1.js
+++ 11.3/static/app_1.js
@@ -48,6 +48,3 @@ function printHello() {
     // just be the original tableData.
     buildTable(filteredData);
   };
-  d3.selectAll("#filter-btn").on("click", handleClick);
-
-  buildTable(tableData);
